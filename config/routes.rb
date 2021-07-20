c5084dae719eb1b8aeca01831f133644da6eebbf
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 08:17:36 +0000

    [Add]homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :owners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  scope :home do
+    get 'about' => 'homes#about'
+  end
 end
