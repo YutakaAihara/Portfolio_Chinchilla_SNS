b2fff7050dff218bb926bf220ec3ae6bc0c28fdd
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 15:17:51 +0000

    [Add]googlemap用js

---
+++ app/assets/javascripts/googlemap.js
@@ -0,0 +1,45 @@
+let map
+let geocoder
+
+function initMap(){
+  // geocoderを初期化
+  geocoder = new google.maps.Geocoder();
+  
+  if(document.getElementById('map')){
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 17
+    });
+  }else{
+    map = new google.maps.Map(document.getElementById('show_map'), {
+      center: {lat: gon.latitude, lng: gon.longitude},
+      zoom: 17
+    });
+  
+    marker = new google.maps.Marker({ 
+      position: {lat: gon.latitude, lng: gon.longitude},
+      map: map
+    });
+  }
+}
+
+function codeAddress(){
+  // 入力を取得
+  let inputAddress = document.getElementById('address').value;
+
+  // geocodingしたあとmapを移動
+  geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+    if (status == 'OK') {
+　　　　　　　　　　　　// map.setCenterで地図が移動
+      map.setCenter(results[0].geometry.location);
+
+　　　　　　　　　　　　// google.maps.MarkerでGoogleMap上の指定位置にマーカが立つ
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
