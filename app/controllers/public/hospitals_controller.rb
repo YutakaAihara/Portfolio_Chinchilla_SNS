189fa91f6658b034aa88f7d0d3821e94c6fad929
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 15:15:21 +0000

    [Add]hospitalコントローラ追記

--- app/controllers/public/hospitals_controller.rb
+++ app/controllers/public/hospitals_controller.rb
@@ -1,13 +1,43 @@
 class Public::HospitalsController < ApplicationController
   def index
-  end
-
-  def new
+    @hospitals = Hospital.all
   end
 
   def show
+    @hospital = Hospital.find(params[:id])
+    gon.latitude = @hospital.latitude
+    gon.longitude = @hospital.longitude
+  end
+
+  def new
+    @hospital = Hospital.new
+  end
+  
+  def create
+    hospital = Hospital.new(hospital_params)
+    hospital.save
+    redirect_to hospital_path(hospital)
   end
 
   def edit
+    @hospital = Hospital.find(params[:id])
+  end
+  
+  def update
+    hospital = Hospital.find(params[:id])
+    hospital.update(hospital_params)
+    redirect_to hospital_path(hospital)
+  end
+  
+  def destroy
+    hospital = Hospital.find(params[:id])
+    hospital.destroy
+    redirect_to hospitals_path
+  end
+  
+  private
+  
+  def hospital_params
+    params.require(:hospital).permit(:hospital_name, :address, :rate, :comment, :latitude, :longitude, :image)
   end
 end
