850648b7beb933c9419c51bd43ef893c197dce18
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 12:41:49 +0000

    [Add]ownerコントローラ追記

--- app/controllers/public/owners_controller.rb
+++ app/controllers/public/owners_controller.rb
@@ -1,10 +1,22 @@
 class Public::OwnersController < ApplicationController
-  def index
-  end
 
   def show
+    @owner = current_owner
   end
 
   def edit
+    @owner = current_owner
+  end
+  
+  def update
+    owner = current_owner
+    owner.update(owner_params)
+    redirect_to owner_path(owner)
+  end
+  
+  private
+  
+  def owner_params
+    params.require(:owner).permit(:name, :email, :prefecture, image)
   end
 end
