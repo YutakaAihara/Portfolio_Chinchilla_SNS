dbfc599482aa3f3d13b842e8169df65abcf602e8
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 15:11:57 +0000

    [Add]hospitalモデル追記

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,12 @@
 class Hospital < ApplicationRecord
   attachment :image
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :rate, numericality: { less_than_or_equal_to: 5,
+                                   greater_than_or_equal_to: 0 }, 
+                   presence: true
+  validates :comment, presence: true
+  
+  geocoded_by :address
+  after_validation :geocode
 end
