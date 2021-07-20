0152352dd1bf4c4ef6d5e0a9834cd5dcad899cc2
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 08:20:30 +0000

    [Update]deviseのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :owners
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :owners, controllers: {
+    sessions:      'owners/sessions',
+    passwords:     'owners/passwords',
+    registrations: 'owners/registrations',
+    confirmations: 'owners/confirmations',
+    omniauth: 'owners/omniauth',
+    unlocks: 'owners/unlocks'
+  }
+  
   root to: 'homes#top'
   scope :home do
     get 'about' => 'homes#about'
