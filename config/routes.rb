6515b0ee8418ff0a4c3ebe05ae0797a1e2ecf0ef
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 08:46:37 +0000

    [Update]ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,6 @@
 Rails.application.routes.draw do
   
-  namespace :public do
-    get 'questions/index'
-    get 'questions/new'
-    get 'questions/show'
-    get 'questions/edit'
-  end
-  namespace :public do
-    get 'hospitals/index'
-    get 'hospitals/new'
-    get 'hospitals/show'
-    get 'hospitals/edit'
-  end
-  namespace :public do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'chinchillas/index'
-    get 'chinchillas/new'
-    get 'chinchillas/show'
-    get 'chinchillas/edit'
-  end
-  namespace :public do
-    get 'owners/index'
-    get 'owners/show'
-    get 'owners/edit'
-  end
+
   devise_for :owners, controllers: {
     sessions:      'owners/sessions',
     passwords:     'owners/passwords',
@@ -37,6 +9,16 @@ Rails.application.routes.draw do
     omniauth: 'owners/omniauth',
     unlocks: 'owners/unlocks'
   }
+
+  scope module: :public do
+    resource :owners, only: [:edit, :update]
+    get 'mypage' => 'owners#show'
+    resources :pets
+    resources :posts
+    resources :hospitals
+    resources :questions
+
+  end
   
   root to: 'homes#top'
   scope :home do
