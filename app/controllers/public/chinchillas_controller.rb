9d276e339d263ff962d8b2478bebafb22eb6092a
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-20 12:30:18 +0000

    [Add]chinchillaコントローラ追記

--- app/controllers/public/chinchillas_controller.rb
+++ app/controllers/public/chinchillas_controller.rb
@@ -1,13 +1,45 @@
 class Public::ChinchillasController < ApplicationController
   def index
+    @chinchillas = Chinchilla.all
   end
 
   def new
+    @chinchilla = Chinchilla.new
+  end
+  
+  def create
+    chinchilla = Chinchilla.new(chinchilla_params)
+    chinchilla.owner_id = current_owner.id
+    if pet.save
+      redirect_to chinchilla_path(chinchilla)
+    end
   end
 
   def show
+    @chinchilla = Chinchilla.find(prams[:id])
   end
 
   def edit
+    @chinchilla = Chinchilla.find(prams[:id])
+  end
+  
+  def update
+    chinchilla = Chinchilla.find(params[:id])
+    if chinchilla.update(chinchilla_params)
+      redirect_to chinchilla_path(chinchilla)
+    end
+  end
+  
+  def destroy
+    chinchilla = Chinchilla.find(params[:id])
+    if chinchilla.destroy
+      redirect_to chinchillas_path
+    end
+  end
+  
+  private
+  
+  def chinchilla_params
+    params.require(:chinchilla).permit(:name, :birthday, :sex, :fur_type, :introduction, :image, :owner_id)
   end
 end
