9425fbd7bedb270e087d03b64b3cc549369ec47d
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-30 08:30:41 +0000

    [Add]質問画像・コメントのバリデーション

--- app/models/question_comment.rb
+++ app/models/question_comment.rb
@@ -1,4 +1,6 @@
 class QuestionComment < ApplicationRecord
   belongs_to :owner
   belongs_to :question
+  
+  validates :comment, presence: true
 end
--- app/models/question_image.rb
+++ app/models/question_image.rb
@@ -1,4 +1,7 @@
 class QuestionImage < ApplicationRecord
   belongs_to :question
   attachment :image
+  
+  validates :image_id, presence: true
+  
 end
