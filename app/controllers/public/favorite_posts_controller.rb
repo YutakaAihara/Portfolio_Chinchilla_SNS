1865d4710824425e4c5022afc97bf6d96359bdb0
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-31 01:21:55 +0000

    [Add]投稿いいね非同期

--- app/controllers/public/favorite_posts_controller.rb
+++ app/controllers/public/favorite_posts_controller.rb
@@ -3,16 +3,14 @@ class Public::FavoritePostsController < ApplicationController
   end
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_owner.favorite_posts.build(post_id: post.id)
-    favorite.save
-    redirect_to post_path(post)  
+    @post = Post.find(params[:post_id])
+    @favorite = current_owner.favorite_posts.build(post_id: @post.id)
+    @favorite.save
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_owner.favorite_posts.find_by(post_id: post.id)
-    favorite.destroy
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @favorite = current_owner.favorite_posts.find_by(post_id: @post.id)
+    @favorite.destroy
   end
 end
