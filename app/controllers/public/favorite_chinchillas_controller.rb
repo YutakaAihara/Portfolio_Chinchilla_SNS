1008bc0bda6bb8ccc97601e08cd9f0c48c45df02
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-31 01:12:55 +0000

    [Add]チンチラいいね非同期

--- app/controllers/public/favorite_chinchillas_controller.rb
+++ app/controllers/public/favorite_chinchillas_controller.rb
@@ -3,16 +3,15 @@ class Public::FavoriteChinchillasController < ApplicationController
   end
 
   def create
-    chinchilla = Chinchilla.find(params[:chinchilla_id])
-    favorite = current_owner.favorite_chinchillas.build(chinchilla_id: chinchilla.id)
-    favorite.save
-    redirect_to chinchilla_path(chinchilla)  
+    @chinchilla = Chinchilla.find(params[:chinchilla_id])
+    @favorite = current_owner.favorite_chinchillas.build(chinchilla_id: @chinchilla.id)
+    @favorite.save
   end
 
   def destroy
-    chinchilla = Chinchilla.find(params[:chinchilla_id])
-    favorite = current_owner.favorite_chinchillas.find_by(chinchilla_id: chinchilla.id)
-    favorite.destroy
-    redirect_to chinchilla_path(chinchilla)
+    @chinchilla = Chinchilla.find(params[:chinchilla_id])
+    @favorite = current_owner.favorite_chinchillas.find_by(chinchilla_id: @chinchilla.id)
+    @favorite.destroy
+
   end
 end
