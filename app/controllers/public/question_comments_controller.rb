8fc274e05cfbfa0d21e41627a0c1519ddd5c99ff
YutakaAihara <youguisufanyuan@gmail.com>
2021-07-31 00:49:53 +0000

    [Add]コメント非同期

--- app/controllers/public/question_comments_controller.rb
+++ app/controllers/public/question_comments_controller.rb
@@ -1,16 +1,17 @@
 class Public::QuestionCommentsController < ApplicationController
   before_action :authenticate_owner!
   def create
-    question = Question.find(params[:question_id])
-    comment = current_owner.question_comments.new(question_comment_params)
-    comment.question_id = question.id
-    comment.save
-    redirect_to question_path(question)
+    @question = Question.find(params[:question_id])
+    @question_comment = current_owner.question_comments.new(question_comment_params)
+    @question_comment.question_id = @question.id
+    @question_comment.save
   end
   
   def destroy
-    QuestionComment.find_by(id: params[:id], question_id: params[:question_id]).destroy
-    redirect_to question_path(params[:question_id])
+    @question = Question.find(params[:question_id])
+    @question_comment = QuestionComment.find_by(id: params[:id], question_id: params[:question_id])
+    @question_comment.destroy
+
   end
   
   private
